5e6bcf6f809df924b1b5aa6cf95bf423f2627c19
jm <jm@DESKTOP-CJP7DQF>
2021-05-20 14:30:13 -0500

    Añadi el codigo fuente

---
+++ src/main/java/dto/Cliente.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dto;
+
+/**
+ *
+ * @author jm
+ */
+public class Cliente {
+    private Long id;
+    private String nombre;
+    private Integer numeroMesa;
+    private Integer cantidadComensales;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getNumeroMesa() {
+        return numeroMesa;
+    }
+
+    public void setNumeroMesa(Integer numeroMesa) {
+        this.numeroMesa = numeroMesa;
+    }
+
+    public Integer getCantidadComensales() {
+        return cantidadComensales;
+    }
+
+    public void setCantidadComensales(Integer cantidadComensales) {
+        this.cantidadComensales = cantidadComensales;
+    }
+    @Override
+        public String toString() {
+            return this.nombre +""+
+                    this.numeroMesa+""+
+                    this.cantidadComensales;
+        }    
+}
---
+++ src/main/java/web/ManejadorBean.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package web;
+
+/**
+ *
+ * @author jm
+ */
+public class ManejadorBean {
+    private Long id;
+    private String nombre;
+    private Integer numeroMesa;
+    private Integer cantidadComensales;
+    private String mensaje;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getNumeroMesa() {
+        return numeroMesa;
+    }
+
+    public void setNumeroMesa(Integer numeroMesa) {
+        this.numeroMesa = numeroMesa;
+    }
+
+    public Integer getCantidadComensales() {
+        return cantidadComensales;
+    }
+
+    public void setCantidadComensales(Integer cantidadComensales) {
+        this.cantidadComensales = cantidadComensales;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+    
+    
+}
